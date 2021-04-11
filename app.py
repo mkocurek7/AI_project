088b9753a150163269408cbd3b87784acb8068e8
mkocurek7 <mkocurek@student.agh.edu.pl>
2021-04-11 22:38:25 +0200

    Initial commit-> obrowka danych, diagramy,statystyczny opis danych

---
+++ app.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import pyplot
+import seaborn as sns
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental import preprocessing
+from sklearn.impute import SimpleImputer
+#from sklearn.preprocessing import Imputer
+from numpy import nan 
+
+#raw_data= read_csv('train.csv') #csv ze wszystkimi rekordami 
+raw_data=read_csv('train2.csv') #csv połączone test.csv+train.csv(bez rekordow gdzie w calym wierszu brakuje danych)
+dataset=raw_data.copy()
+
+#print(dataset.shape) #shape zwraca (ilosc wierszy, ilosc kolumn)
+#print(dataset.isna().sum()) #zwraca ilosc danych ktore sa nieprawidlowe
+
+#łatwe sprawdzenie zmiennych kategorycznych jakie sa
+table=pd.pivot_table(dataset, values='quantity', index=['city', 'shop', 'brand'], columns='container',  aggfunc=np.sum)
+#print(table)
+
+#jesli miałby sie czepiac ze dane lekko zmodyfikowane a nie surowe z kaggle:
+#pobranie danych z train.csv i tets.csv-> TODO:
+#dataset = dataset.dropna() #usuwa nieprawidlowe wiersze z danymi (usuwa takze wiersz gdzie 1 dana jest nieprawidlowa)
+#usunac jak najmniejsza ilosc rekordow-> reszte ladnie zastąpic-> jesli sie da , do przemyslenia
+#no i jesli sie da połączyc train i test i jakos zczytac
+
+
+#organizacja zmiennych jakosciowych-> zamiast nazw sa liczby
+city_map={'Athens':1,'Irakleion':2,'Larisa':3,'Patra':4, 'Thessaloniki':5 }
+shop_map={'shop_1':1, 'shop_2':2, 'shop_3':3,'shop_4':4, 'shop_5':5,'shop_6':6}
+brand_map={'kinder-cola':1, 'adult-cola':2, 'orange-power':3, 'gazoza':4,'lemon-boost':5}
+container_map={'can':1, 'glass':2,'plastic':3}
+dataset['city']=dataset['city'].map(city_map)
+dataset['shop']=dataset['shop'].map(shop_map)
+dataset['brand']=dataset['brand'].map(brand_map)
+dataset['container']=dataset['container'].map(container_map)
+
+#brakujace dane i uzupelnianie
+print(dataset.isnull().sum()) # do sprawdzenia gdzie brakuje danych
+
+#jakby byl problem z nullami to odkomentowac konwersje na NaN
+#dataset[['lat','long','container','capacity']]=dataset[['lat','long','container','capacity',]].replace(0,np.nan)
+#dataset['capacity']=dataset['capacity'].replace(0,np.nan)
+
+#wypelnienie brkaujacych danych
+dataset.fillna(dataset.mean(), inplace=True)
+dataset.fillna(dataset['capacity'].mode()[0], inplace=True)
+print(dataset.isnull().sum())
+
+#wypisanie linijek z brakujacymi danymi jesli takie sie pojawią
+#null_data = dataset[dataset.isnull().any(axis=1)]
+#print(null_data)
+
+
+print(dataset.describe()) #opis statystyczny danych :)
+
+#DIAGRAMY-> TODO: tak zeby nie brało wszystkich kolumn typu id...
+    # wykresy wasy-box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(10,10), sharex=False, sharey=False)
+plt.show()
+
+    #histogramy
+dataset.hist()
+plt.show()
+
+#scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
+
+#podzial na train i test
+train_dataset= dataset.sample(frac=0.8, random_state=0)
+test_dataset=dataset.drop(train_dataset.index)
+
+
+
