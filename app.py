4ee2afb017ed90bb3071583a437dd8781f16c809
Jakub Kopeć <Lasiuk16@gmail.com>
2021-04-30 11:34:41 +0200

    Rozpoczecie uczenia

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import np as np
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,6 +17,14 @@ from tensorflow.keras.layers.experimental import preprocessing
 from sklearn.impute import SimpleImputer
 # from sklearn.preprocessing import Imputer
 from numpy import nan
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import tensorflow as tf
+tf.debugging.set_log_device_placement(True)
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+import warnings
+warnings.filterwarnings(action='ignore')
 
 #raw_data= read_csv('train.csv') #csv ze wszystkimi rekordami 
 raw_data=read_csv('train2.csv') #csv połączone test.csv+train.csv(bez rekordow gdzie w calym wierszu brakuje danych)
@@ -40,10 +49,12 @@ city_map={'Athens':1,'Irakleion':2,'Larisa':3,'Patra':4, 'Thessaloniki':5 }
 shop_map={'shop_1':1, 'shop_2':2, 'shop_3':3,'shop_4':4, 'shop_5':5,'shop_6':6}
 brand_map={'kinder-cola':1, 'adult-cola':2, 'orange-power':3, 'gazoza':4,'lemon-boost':5}
 container_map={'can':1, 'glass':2,'plastic':3}
+container_volume_map={'1.5lt':1,'330ml':2,'500ml':3}
 dataset['city']=dataset['city'].map(city_map)
 dataset['shop']=dataset['shop'].map(shop_map)
 dataset['brand']=dataset['brand'].map(brand_map)
 dataset['container']=dataset['container'].map(container_map)
+dataset['capacity']=dataset['capacity'].map(container_volume_map)
 
 #brakujace dane i uzupelnianie
 print(dataset.isnull().sum()) # do sprawdzenia gdzie brakuje danych
@@ -64,70 +75,84 @@ print(dataset.isnull().sum())
 print(dataset.describe()) #opis statystyczny danych :)
 
 #DIAGRAMY
-print("")
-#wykresy wasy-box 
-dataset.columns
-print('pop')
-plt.boxplot(dataset['pop'])
-plt.show()
-print('price')
-plt.boxplot(dataset['price'])
-plt.show()
-print('quantity')
-plt.boxplot(dataset['quantity'])
-plt.show()
-
-#histogramy (lat-> latitude , long-> longitude)
-dataset.hist('pop')
-plt.show()
-dataset.hist('price')
-plt.show()
-dataset.hist('quantity')
-plt.show()
-dataset.hist('city')
-plt.show()
-dataset.hist('brand')
-plt.show()
-dataset.hist('container')
-plt.show()
-#przy container,brand, city konkretne wartosci typu 1 ,2 oznaczaja miasta/rodzaj opakowania/marke
-
-sns.relplot(x=dataset['price'], y=dataset['container'])
-sns.relplot(x=dataset['price'], y=dataset['brand'])
-sns.relplot(x=dataset['price'], y=dataset['quantity']) #zaleznosc cena ilosc 
-sns.relplot(x=dataset['city'], y=dataset['pop']) 
-sns.relplot(x=dataset['city'], y=dataset['quantity']) 
-
-#podzial na train i test
-#train_dataset= dataset.sample(frac=0.8, random_state=0)
-#test_dataset=dataset.drop(train_dataset.index)
-
-# wstepnie do wywalenia
-# rng = RandomState()
-# train_dataset = raw_data.sample(frac=0.8, random_state=rng)
-# test_dataset = raw_data.sample[~dataset.index.isin(train_dataset.index)]
-
-# wstepnie do wywalenia
-# train_dataset, test_dataset = train_test_split(dataset, train_size=0.8, test_size=0.2, random_state=None, shuffle=False)
-# print(train_dataset.shape)
-# print(test_dataset.shape)
+#print("")
+##wykresy wasy-box
+#dataset.columns
+#print('pop')
+#plt.boxplot(dataset['pop'])
+#plt.show()
+#print('price')
+#plt.boxplot(dataset['price'])
+#plt.show()
+#print('quantity')
+#plt.boxplot(dataset['quantity'])
+#plt.show()
+#
+##histogramy (lat-> latitude , long-> longitude)
+#dataset.hist('pop')
+#plt.show()
+#dataset.hist('price')
+#plt.show()
+#dataset.hist('quantity')
+#plt.show()
+#dataset.hist('city')
+#plt.show()
+#dataset.hist('brand')
+#plt.show()
+#dataset.hist('container')
+#plt.show()
+##przy container,brand, city konkretne wartosci typu 1 ,2 oznaczaja miasta/rodzaj opakowania/marke
+#
+#sns.relplot(x=dataset['price'], y=dataset['container'])
+#sns.relplot(x=dataset['price'], y=dataset['brand'])
+#sns.relplot(x=dataset['price'], y=dataset['quantity']) #zaleznosc cena ilosc
+#sns.relplot(x=dataset['city'], y=dataset['pop'])
+#sns.relplot(x=dataset['city'], y=dataset['quantity'])
 
 # quantity jest zmienna wyjsciowa wiec wywalamy ja ze zbioru x i wrzucamy do zbioru y
+#lat i long nie wydaja sie potrzebne do czegokolwiek
 # axis=1 powoduje ze indeksy nie beda uwzgldniane podczas trenowania
 
-# nie wiem czy daty tez nie wyrzucic z x
-x = dataset.drop(["quantity", "date"], axis=1)
-y = dataset["quantity"]
+#X = dataset.drop("quantity", axis=1).copy()
+#y = dataset["quantity"].copy()
+#x_train, x_test, y_train, y_test = ms.train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=42)
 
-dataset["quantity"].hist()
+dataset = dataset.astype({"pop": "int"})
+dataset = dataset.astype({"price": "float64"})
+dataset = dataset.astype({"quantity": "int"})
+dataset = dataset.astype({"capacity": "int"})
+dataset = dataset.astype({"city": "int"})
+dataset = dataset.astype({"brand": "int"})
+dataset = dataset.astype({"container": "int"})
+dataset = dataset.astype({"shop": "int"})
 
-y -= 1
+dataset.pop("lat")
+dataset.pop("long")
+dataset.pop("date")
 
-print(dataset["quantity"].value_counts())
+dataset.info()
 
-#opcjonalnie - zaokraglenie danych w kolumnie quatity tak by bylo mniej przedzialow  TODO:
+x = dataset.drop('quantity', axis=1).copy()
+y = dataset['quantity'].copy()
 
-x_train, x_test, y_train, y_test = ms.train_test_split(x, y, train_size=0.8, test_size=0.2, random_state=None, shuffle=False) #, stratify=y
+y-=1
+#print(y)
+
+x_train,x_test,y_train,y_test = train_test_split(x, y, train_size=0.8, test_size=0.2, random_state=42, shuffle=False)
+y_train.hist()
+plt.ylabel('Y_TRAIN')
+plt.show()
+
+#alternatywa
+#train_dataset, test_dataset = train_test_split(dataset, train_size=0.8, test_size=0.2, random_state=None, shuffle=False)
+#x_train = train_dataset.copy()
+#x_test = test_dataset.copy()
+#y_train = x_train.pop('quantity')
+#y_test = x_test.pop('quantity')
+#normalizer = preprocessing.Normalization()
+#normalizer.adapt(np.array(x_train))
+#print(normalizer.mean.numpy())
+#print(y_test)
 
 model = keras.models.Sequential([
     # definicja pierwszej wartwy - argumentem jest liczba zmiennych wejsciowych
@@ -138,10 +163,8 @@ model = keras.models.Sequential([
     keras.layers.Dense(400, activation="relu"),
     keras.layers.Dense(400, activation="relu"),
     keras.layers.Dense(200, activation="relu"),
-    keras.layers.Dense(150, activation="relu"),
-    # definicja warstwy wejsciowej
-    # pierszy argument - liczba neuronow wyjsciowych (prawdopodobnie powinny byc 3)
-    keras.layers.Dense(3, activation="softmax")
+    # pierszy argument - liczba neuronow wyjsciowych
+    keras.layers.Dense(400000, activation="softmax")
 ])
 
 model.compile(
@@ -152,14 +175,9 @@ model.compile(
     # metryki procesu uczenia
     metrics=["accuracy"]
 )
-
-
-print(x.value_counts())
 model.summary()
-#zmapowac opakowania o danej pojemnosci do intow
-x_train=np.asarray(x_train).astype(np.int)
-y_train=np.asarray(y_train).astype(np.int)
-model.fit(x_train, y_train, epochs=300, validation_data=(x_test, y_test))
 
+#model_history = model.fit(x_train, y_train, epochs=100, validation_data=(x_test, y_test))
 
-
+#plt.plot(model_history.history["val_accuracy"])
+#np.max(model_history.history["accuracy"])
